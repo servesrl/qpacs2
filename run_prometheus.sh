eb886e08c0fd002730a5a7b648f8c6bdce071abd
chpiero <piero.chiacchiaretta@gmail.com>
2019-07-26 23:46:49 +0200

    fix time log, add Tools, add/mod command

---
+++ run_prometheus.sh
@@ -0,0 +1,1 @@
+docker run -p 9090:9090 -v /home/piero/dev/qpcas2/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml --rm prom/prometheus:v2.7.0
--- run_qpacs2.sh
+++ run_qpacs2.sh
@@ -1,1 +1,9 @@
-docker-compose up -d
+#!/bin/bash
+docker-compose up -d 
+#for container in nginx mongo postgres orthanc viewer prometheus 
+#for container in nginx mongo postgres orthanc
+#do
+# docker-compose exec $container rm -f /etc/localtime
+# docker-compose exec $container ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime
+#done
+docker-compose ps
